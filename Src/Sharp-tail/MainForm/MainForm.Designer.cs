bf3f6b02d71debbb7591db58a0184f0c9f4b1201
John Larsen <john@pcis.se>
2019-07-06 10:17:09 +0200

    WIP adding tabs

--- Src/Sharp-tail/MainForm/MainForm.Designer.cs
+++ Src/Sharp-tail/MainForm/MainForm.Designer.cs
@@ -28,124 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.toolStripStatusLabelName = new System.Windows.Forms.ToolStripStatusLabel();
-            this.toolStripStatusLabelSize = new System.Windows.Forms.ToolStripStatusLabel();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
-            this.mainTextBox1 = new ListBoxControl.Controls.MainTextBox();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.toolStripMenuItemColorRule = new System.Windows.Forms.ToolStripMenuItem();
-            this.statusStrip1.SuspendLayout();
-            this.menuStrip1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // statusStrip1
-            // 
-            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabelName = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStripStatusLabelSize = new System.Windows.Forms.ToolStripStatusLabel();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItemColorRule = new System.Windows.Forms.ToolStripMenuItem();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+			this.mainTextBox1 = new ListBoxControl.Controls.MainTextBox();
+			this.statusStrip1.SuspendLayout();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabelName,
             this.toolStripStatusLabelSize});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 844);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(1201, 37);
-            this.statusStrip1.TabIndex = 0;
-            this.statusStrip1.Text = "statusStrip1";
-            // 
-            // toolStripStatusLabelName
-            // 
-            this.toolStripStatusLabelName.Name = "toolStripStatusLabelName";
-            this.toolStripStatusLabelName.Size = new System.Drawing.Size(126, 32);
-            this.toolStripStatusLabelName.Text = "Name: n/a";
-            // 
-            // toolStripStatusLabelSize
-            // 
-            this.toolStripStatusLabelSize.Name = "toolStripStatusLabelSize";
-            this.toolStripStatusLabelSize.Size = new System.Drawing.Size(83, 32);
-            this.toolStripStatusLabelSize.Text = "Size: 0";
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.statusStrip1.Location = new System.Drawing.Point(0, 901);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 7, 0);
+			this.statusStrip1.Size = new System.Drawing.Size(1264, 22);
+			this.statusStrip1.TabIndex = 0;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// toolStripStatusLabelName
+			// 
+			this.toolStripStatusLabelName.Name = "toolStripStatusLabelName";
+			this.toolStripStatusLabelName.Size = new System.Drawing.Size(63, 17);
+			this.toolStripStatusLabelName.Text = "Name: n/a";
+			// 
+			// toolStripStatusLabelSize
+			// 
+			this.toolStripStatusLabelSize.Name = "toolStripStatusLabelSize";
+			this.toolStripStatusLabelSize.Size = new System.Drawing.Size(39, 17);
+			this.toolStripStatusLabelSize.Text = "Size: 0";
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripMenuItem1,
             this.toolStripMenuItemColorRule});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1201, 42);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // toolStripMenuItem1
-            // 
-            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Padding = new System.Windows.Forms.Padding(3, 1, 0, 1);
+			this.menuStrip1.Size = new System.Drawing.Size(1264, 24);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.openToolStripMenuItem,
             this.exitToolStripMenuItem});
-            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(64, 36);
-            this.toolStripMenuItem1.Text = "File";
-            // 
-            // openToolStripMenuItem
-            // 
-            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(173, 38);
-            this.openToolStripMenuItem.Text = "Open";
-            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(173, 38);
-            this.exitToolStripMenuItem.Text = "Exit";
-            // 
-            // elementHost1
-            // 
-            this.elementHost1.AllowDrop = true;
-            this.elementHost1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.elementHost1.Location = new System.Drawing.Point(0, 42);
-            this.elementHost1.Name = "elementHost1";
-            this.elementHost1.Size = new System.Drawing.Size(1201, 802);
-            this.elementHost1.TabIndex = 2;
-            this.elementHost1.Text = "elementHost1";
-            this.elementHost1.Child = this.mainTextBox1;
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // toolStripMenuItemColorRule
-            // 
-            this.toolStripMenuItemColorRule.Name = "toolStripMenuItemColorRule";
-            this.toolStripMenuItemColorRule.Size = new System.Drawing.Size(142, 38);
-            this.toolStripMenuItemColorRule.Text = "Color rules";
-            this.toolStripMenuItemColorRule.Click += new System.EventHandler(this.toolStripMenuItemColorRule_Click);
-            // 
-            // MainForm
-            // 
-            this.AllowDrop = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1201, 881);
-            this.Controls.Add(this.elementHost1);
-            this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.menuStrip1);
-            this.KeyPreview = true;
-            this.MainMenuStrip = this.menuStrip1;
-            this.Name = "MainForm";
-            this.Text = "Sharp-tail";
-            this.Load += new System.EventHandler(this.MainForm_Load);
-            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
-            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
-            this.Resize += new System.EventHandler(this.MainForm_Resize);
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 22);
+			this.toolStripMenuItem1.Text = "File";
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+			this.openToolStripMenuItem.Text = "Open";
+			this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+			this.exitToolStripMenuItem.Text = "Exit";
+			// 
+			// toolStripMenuItemColorRule
+			// 
+			this.toolStripMenuItemColorRule.Name = "toolStripMenuItemColorRule";
+			this.toolStripMenuItemColorRule.Size = new System.Drawing.Size(76, 22);
+			this.toolStripMenuItemColorRule.Text = "Color rules";
+			this.toolStripMenuItemColorRule.Click += new System.EventHandler(this.toolStripMenuItemColorRule_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Location = new System.Drawing.Point(365, 83);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(637, 489);
+			this.tabControl1.TabIndex = 3;
+			// 
+			// elementHost1
+			// 
+			this.elementHost1.Location = new System.Drawing.Point(54, 96);
+			this.elementHost1.Name = "elementHost1";
+			this.elementHost1.Size = new System.Drawing.Size(200, 100);
+			this.elementHost1.TabIndex = 4;
+			this.elementHost1.Text = "elementHost1";
+			this.elementHost1.Child = this.mainTextBox1;
+			// 
+			// MainForm
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1264, 923);
+			this.Controls.Add(this.elementHost1);
+			this.Controls.Add(this.tabControl1);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.menuStrip1);
+			this.KeyPreview = true;
+			this.MainMenuStrip = this.menuStrip1;
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Name = "MainForm";
+			this.Text = "Sharp-tail";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+			this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+			this.Resize += new System.EventHandler(this.MainForm_Resize);
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -156,12 +167,14 @@
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelSize;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
-        private System.Windows.Forms.Integration.ElementHost elementHost1;
-        private ListBoxControl.Controls.MainTextBox mainTextBox1;
+        //private ListBoxControl.Controls.MainTextBox mainTextBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemColorRule;
-    }
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.Integration.ElementHost elementHost1;
+		private ListBoxControl.Controls.MainTextBox mainTextBox1;
+	}
 }
 
--- Src/Sharp-tail/MainForm/MainForm.cs
+++ Src/Sharp-tail/MainForm/MainForm.cs
@@ -1,24 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
+using System.Windows.Forms.Integration;
 using Common;
 using Common.Models;
+using ListBoxControl.Controls;
+using DataFormats = System.Windows.Forms.DataFormats;
+using DragDropEffects = System.Windows.Forms.DragDropEffects;
+using DragEventArgs = System.Windows.Forms.DragEventArgs;
+using Point = System.Windows.Point;
 
 namespace MainForm
 {
     public partial class MainForm : Form
     {
+		//TabClose Icon 
+		private System.Drawing.Point _ImageLocation = new System.Drawing.Point(19, 5);
+		private System.Drawing.Point _imageHitArea = new System.Drawing.Point(19,2);
+		Image CloseImage;
+
+
         private FileInfo fInfo;
         private bool FullScreen = false;
         private bool ctrlDown = false;
         private List<ColorRule> ColorRules;
+        private Dictionary<string, string> tabpages;
 
         public MainForm()
         {
             InitializeComponent();
             ColorRules = ColorRuleSerializer.Load();
-
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
@@ -46,9 +59,42 @@ namespace MainForm
         {
             fInfo = new FileInfo(file);
             SetFileAtrubutesToGui();
-            mainTextBox1.SetDataFile(file, ColorRules);
-            MainForm_Resize(this, null);
+			CreateTab(file);
+
+            //mainTextBox1.SetDataFile(file, ColorRules);
+            //MainForm_Resize(this, null);
         }
+
+        private void CreateTab(string file)
+        {
+	        
+	        int lastIndex = file.LastIndexOf('\\');
+	        var tabpageName = new TabPage(file.Substring(lastIndex+1));
+	        var textbox = new MainTextBox
+	        {
+		        Name = "TextBox"
+	        };
+	        
+			var host = new ElementHost
+			{
+				Name="Host",
+				Dock=DockStyle.Fill,
+				Child=textbox
+			};
+
+			textbox.SetDataFile(file, ColorRules);
+			tabpageName.Controls.Add(host);
+			tabControl1.TabPages.Add(tabpageName);
+			textbox.SetSize(textbox.ActualWidth, textbox.ActualHeight);
+			//TabControl1_DrawItem(this ,null);
+			//var test = tabControl1.TabPages[0].Controls.Find("TextBox", true);
+			////var test1 = (ElementHost)tabControl1.TabPages[0].Controls.Find("Host", true);
+			//var test1 = (ElementHost) tabControl1.TabPages[0].Controls[0];
+			//var tBox = (MainTextBox)test1.Child;
+			//tBox.Name = "Gurka";
+
+        }
+
 
         private void MainForm_Resize(object sender, EventArgs e)
         {
@@ -74,10 +120,48 @@ namespace MainForm
             mainTextBox1.PreviewKeyDown += MainTextBox1_PreviewKeyDown;
             mainTextBox1.PreviewKeyUp += MainTextBox1_PreviewKeyUp;
             mainTextBox1.PreviewMouseWheel += MainTextBox1_MouseWheel;
+
+			//TODO: if this work make code parmanet Tab controle stuff 
+			tabControl1.DrawMode=TabDrawMode.OwnerDrawFixed;
+			tabControl1.DrawItem+=TabControl1_DrawItem;
+			CloseImage = Properties.Resources.close;
+			//Look in to this if image is wrong 
+			tabControl1.Padding = new System.Drawing.Point(20,5);
+
         }
 
-        private void MainTextBox1_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+		private void TabControl1_DrawItem(object sender, DrawItemEventArgs e)
+		{
+			try
+			{
+				Image img = new Bitmap(CloseImage);
+				
+				Rectangle r = e.Bounds;
+				r = tabControl1.GetTabRect(e.Index);
+				r.Offset(2,2);
+				Brush titleBrush = new SolidBrush(Color.Black);
+				Font font = Font;
+				string title = tabControl1.TabPages[e.Index].Text;
+
+				e.Graphics.DrawString(title, font, titleBrush, new PointF(r.X, r.Y));
+
+				var point = new System.Drawing.Point(r.X+(tabControl1.GetTabRect(e.Index).Width-_ImageLocation.X), _ImageLocation.Y);
+					
+				if (tabControl1.SelectedIndex >= 1)
+				{
+					e.Graphics.DrawImage(img, point);
+				}
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception);
+				throw;
+			}
+		}
+
+		private void MainTextBox1_MouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
         {
+			//TODO: adjust this to Tabcontrole
             if (ctrlDown)
             {
                 var direction = Math.Sign(e.Delta);
@@ -163,8 +247,19 @@ namespace MainForm
             if (result == DialogResult.OK)
             {
                 ColorRules = cf.ColorRules;
-                mainTextBox1.UpdateColorRules(ColorRules);
+                //mainTextBox1.UpdateColorRules(ColorRules);
+				UpdateTextBoxColorRule();
             }
         }
+
+        private void UpdateTextBoxColorRule()
+        {
+	        foreach (TabPage tabControl1TabPage in tabControl1.TabPages)
+	        {
+		        var host = (ElementHost) tabControl1TabPage.Controls[0];
+		        var textBox = (MainTextBox) host.Child;
+				textBox.UpdateColorRules(ColorRules);
+	        }
+		}
     }
 }
