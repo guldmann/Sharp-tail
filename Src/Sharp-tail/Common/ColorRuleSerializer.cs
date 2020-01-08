ff9081e572a9c323384e87f882d7ec6cd79692da
John Larsen <john.larsen@lindex.com>
2020-01-08 14:50:41 +0100

    Forcing colorrule.json to application root folder

--- Src/Sharp-tail/Common/ColorRuleSerializer.cs
+++ Src/Sharp-tail/Common/ColorRuleSerializer.cs
@@ -2,16 +2,21 @@
 using Common.Models;
 using Newtonsoft.Json;
 using System.IO;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace Common
 {
     public static class ColorRuleSerializer
     {
+        private static readonly string Root = Application.StartupPath;
+        private const string FileName = "colorSettings.json";
+
         public static void Save(this List<ColorRule> colorRules)
         {
             JsonSerializer jsonSerializer = new JsonSerializer();
             jsonSerializer.Formatting = Formatting.Indented;
-            using (StreamWriter sw = new StreamWriter("colorSettings.json"))
+            using (StreamWriter sw = new StreamWriter(Path.Combine(Root, FileName)))
             {
                 using (JsonWriter writer = new JsonTextWriter(sw))
                 {
@@ -22,9 +27,9 @@ namespace Common
 
         public static List<ColorRule> Load()
         {
-            if (File.Exists("colorSettings.json"))
+            if (File.Exists(Path.Combine(Root, FileName)))
             {
-                var jstring = File.ReadAllText("colorSettings.json");
+                var jstring = File.ReadAllText(Path.Combine(Root, FileName));
 
                 return JsonConvert.DeserializeObject<List<ColorRule>>(jstring);
             }
