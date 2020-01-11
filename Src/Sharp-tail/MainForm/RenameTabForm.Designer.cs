99d4831994573d30625696ec719bc73e057f2a51
John Larsen <john@pcis.se>
2020-01-11 16:59:07 +0100

    #80 Added flowpanel to rename tab

--- Src/Sharp-tail/MainForm/RenameTabForm.Designer.cs
+++ Src/Sharp-tail/MainForm/RenameTabForm.Designer.cs
@@ -32,31 +32,37 @@
             this.textBoxTabName = new System.Windows.Forms.TextBox();
             this.buttonOk = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(6, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.Size = new System.Drawing.Size(117, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Tab Name:";
             // 
             // textBoxTabName
             // 
-            this.textBoxTabName.Location = new System.Drawing.Point(78, 6);
+            this.textBoxTabName.Location = new System.Drawing.Point(6, 31);
+            this.textBoxTabName.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBoxTabName.Name = "textBoxTabName";
-            this.textBoxTabName.Size = new System.Drawing.Size(252, 20);
+            this.textBoxTabName.Size = new System.Drawing.Size(500, 31);
             this.textBoxTabName.TabIndex = 1;
             this.textBoxTabName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxTabName_KeyDown);
             // 
             // buttonOk
             // 
+            this.buttonOk.AutoSize = true;
             this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.buttonOk.Location = new System.Drawing.Point(174, 32);
+            this.buttonOk.Location = new System.Drawing.Point(6, 74);
+            this.buttonOk.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonOk.Name = "buttonOk";
-            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.Size = new System.Drawing.Size(150, 44);
             this.buttonOk.TabIndex = 2;
             this.buttonOk.Text = "Ok";
             this.buttonOk.UseVisualStyleBackColor = true;
@@ -64,28 +70,43 @@
             // 
             // buttonCancel
             // 
+            this.buttonCancel.AutoSize = true;
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(255, 32);
+            this.buttonCancel.Location = new System.Drawing.Point(168, 74);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.Size = new System.Drawing.Size(150, 44);
             this.buttonCancel.TabIndex = 3;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
             this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.textBoxTabName);
+            this.flowLayoutPanel1.Controls.Add(this.buttonOk);
+            this.flowLayoutPanel1.Controls.Add(this.buttonCancel);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(6, 6);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(20);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(568, 205);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
             // RenameTabForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 68);
-            this.Controls.Add(this.buttonCancel);
-            this.Controls.Add(this.buttonOk);
-            this.Controls.Add(this.textBoxTabName);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(580, 217);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "RenameTabForm";
+            this.Padding = new System.Windows.Forms.Padding(6);
             this.Text = "Rename tab";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -95,5 +116,6 @@
         private System.Windows.Forms.TextBox textBoxTabName;
         private System.Windows.Forms.Button buttonOk;
         private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
