12bd59716549b72b7993048ccc37d2615014a5d3
John Larsen <john.larsen@lindex.com>
2020-03-04 17:37:49 +0100

    Starrted adding groups

--- Src/Sharp-tail/Common/Models/TabFile.cs
+++ Src/Sharp-tail/Common/Models/TabFile.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
-using Common.Helpers;
+﻿using Common.Helpers;
 
 namespace Common.Models
 {
