071d076d3ab405bd581cbd78adbc236a9cd14804
John Larsen <john@pcis.se>
2020-06-29 07:36:26 +0200

    WIP

--- Src/Sharp-tail/Sharp-Tail-Wpf/LogView.xaml.cs
+++ Src/Sharp-tail/Sharp-Tail-Wpf/LogView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,17 +12,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Sharp_Tail_Wpf.Annotations;
 
 namespace Sharp_Tail_Wpf
 {
     /// <summary>
     /// Interaction logic for LogView.xaml
     /// </summary>
-    public partial class LogView : UserControl
+    public partial class LogView : UserControl, INotifyPropertyChanged
     {
         public LogView()
         {
             InitializeComponent();
+            //  if (text != null) someText = text;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _someText;
+
+        public string someText
+        {
+            get => _someText;
+            set
+            {
+                _someText = value;
+                OnPropertyChanged("someText");
+            }
         }
     }
 }
